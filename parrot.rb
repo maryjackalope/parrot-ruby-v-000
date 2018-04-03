b60e7e8776bd823a63265b88283bfdbc2cb54a7f
Mary Jack <maryjackinthebox@gmail.com>
2018-04-03 00:34:20 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot (speech = "Squawk!")
-  puts "Squawk!"
+def parrot (utterance = "Squawk!")
+  puts utterance
+  utterance
 end
